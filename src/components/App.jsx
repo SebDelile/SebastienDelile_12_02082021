ef18803479c78bede82c400febac274e97a1197c
Delile Sebastien <sebastiendelile@gmail.com>
2021-08-06 08:58:09 +0200

    feat: add router and global style
    add router to App, set the route
    add global style, include font, css reset and css variables

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,38 @@
 import { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import styled from 'styled-components';
+import { Dashboard } from './Dashboard.jsx';
+import { NavTop } from './NavTop.jsx';
+import { NavLeft } from './NavLeft.jsx';
+import { ErrorPage } from './ErrorPage.jsx';
 
-class App extends Component {
+export class App extends Component {
   render() {
-    return <div className="App">Hello world!</div>;
+    return (
+      <AppContainer>
+        <BrowserRouter>
+          <NavTop />
+          <NavLeft />
+          <Switch>
+            <Route
+              path="/:id"
+              render={(routeProps) => <Dashboard props={{ ...routeProps }} />}
+            />
+            <Route>
+              <ErrorPage />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </AppContainer>
+    );
   }
 }
 
-export default App;
+const AppContainer = styled.div`
+  width: 100%;
+  min-height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: stretch;
+`;
--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,121 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
+import { App } from './components/App.jsx';
 import reportWebVitals from './utils/reportWebVitals';
+import { createGlobalStyle } from 'styled-components';
+import robotoRegular1 from './fonts/roboto-regular-webfont.woff';
+import robotoRegular2 from './fonts/roboto-regular-webfont.woff2';
+import robotoMedium1 from './fonts/roboto-medium-webfont.woff';
+import robotoMedium2 from './fonts/roboto-medium-webfont.woff2';
+import robotoBold1 from './fonts/roboto-bold-webfont.woff';
+import robotoBold2 from './fonts/roboto-bold-webfont.woff2';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function initializeApp() {
+  ReactDOM.render(
+    <React.StrictMode>
+      <GlobalStyle />
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+const GlobalStyle = createGlobalStyle`
+/* --------------------------------------------------
+----------------------- Fonts -----------------------
+-------------------------------------------------- */
+
+@font-face {
+  font-family: 'Roboto';
+  src:
+    local('Roboto'),
+    url(${robotoRegular2}) format('woff2'),
+    url(${robotoRegular1}) format('woff');
+  font-weight: 400;
+}
+
+@font-face {
+  font-family: 'Roboto';
+  src:
+    local('Roboto'),
+    url(${robotoMedium2}) format('woff2'),
+    url(${robotoMedium1}) format('woff');
+  font-weight: 500;
+}
+
+@font-face {
+  font-family: 'Roboto';
+  src:
+    local('Roboto'),
+    url(${robotoBold2}) format('woff2'),
+    url(${robotoBold1}) format('woff');
+  font-weight: 700;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+  font-weight: 500;
+}
+
+/* --------------------------------------------------
+--------------------- Variables ---------------------
+-------------------------------------------------- */
+
+:root {
+  --nav-left-width: 7.5rem;
+  --main-padding: 5%;
+
+  @media only screen and (min-width: 90rem) {
+    --main-padding: calc((100% - 90rem * 0.9) / 2);
+  }
+}
+
+/* --------------------------------------------------
+--------------------- CSS reset ---------------------
+-------------------------------------------------- */
+
+*,
+*::after,
+*::before {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+html,
+body,
+#root {
+  height: 100%;
+}
+
+a {
+  text-decoration: inherit;
+  color: inherit;
+}
+
+button {
+  background: inherit;
+  border: inherit;
+  cursor: pointer;
+}
+
+/* --------------------------------------------------
+------------------- Screen-reader -------------------
+-------------------------------------------------- */
+
+.sr-only {
+  border: 0;
+  clip: rect(0 0 0 0);
+  height: 1px;
+  margin: -1px;
+  overflow: hidden;
+  padding: 0;
+  position: absolute;
+  width: 1px;
+}
+`;
+
+initializeApp();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
