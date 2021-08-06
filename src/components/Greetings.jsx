1eab78827a662dd00d9f083fbcf114f86077c855
Delile Sebastien <sebastiendelile@gmail.com>
2021-08-06 15:55:09 +0200

    feat: add Greetings component
    add Greeting component and style

--- src/components/Greetings.jsx
+++ src/components/Greetings.jsx
@@ -4,13 +4,36 @@ import styled from 'styled-components';
 export class Greetings extends Component {
   constructor(props) {
     super(props);
-    this.stuff = null;
+    this.firstName = this.props.data.firstName;
   }
   render() {
-    return <Container>Greetings</Container>;
+    return (
+      <Container>
+        <Title>
+          Bonjour <FirstName>{this.firstName}</FirstName>
+        </Title>
+        <Message>
+          Félicitation ! Vous avez explosé vos objectifs hier 👏
+        </Message>
+      </Container>
+    );
   }
 }
 
 const Container = styled.header`
   grid-area: greetings;
+  margin: 1rem 0;
+`;
+
+const Title = styled.h1`
+  font-size: 3rem;
+  margin-bottom: 1.5rem;
+`;
+
+const FirstName = styled.span`
+  color: red;
+`;
+
+const Message = styled.p`
+  font-size: 1.125rem;
 `;
